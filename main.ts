43f37543ed21fe41b7fe77e4f5f140308aecfb45
strobie <0xstrobe@protonmail.com>
2023-01-07 22:44:09 +0100

    fix

--- main.ts
+++ main.ts
@@ -20,25 +20,13 @@ const S3 = new S3Client({
   },
 });
 
-interface UploadHeaders {
-  "content-type": string;
-  "service-key": string;
-}
-
-interface DeleteHeaders {
-  "service-key": string;
-}
-
 const server = http.createServer();
 
-server.on("request", (request, response) => {
+server.on("request", async (request, response) => {
   const headers = request.headers;
-  // const url = new URL(request.url!);
-  // const pathname = url.pathname;
-  const pathname = request.url!;
+  // remove query string and first slash
+  const pathname = request.url!.split("?")[0].slice(1);
   console.log(pathname);
-
-  let body = "";
 
   if (headers["service-key"] !== SERVICE_KEY) {
     response.statusCode = 403;
@@ -54,8 +42,10 @@ server.on("request", (request, response) => {
   });
 
   if (request.method === "PUT") {
+    // receive the body as a buffer
+    let body: Buffer = Buffer.alloc(0);
     request.on("data", (chunk) => {
-      body += chunk;
+      body = Buffer.concat([body, chunk]);
     });
 
     request.on("end", async () => {
@@ -66,21 +56,18 @@ server.on("request", (request, response) => {
         ContentType: headers["content-type"] as string,
       });
       const sent = await S3.send(command);
-      console.log(sent);
+      console.log(sent.ChecksumSHA1);
       response.statusCode = 200;
       response.end();
     });
   } else if (request.method === "DELETE") {
-    request.on("end", async () => {
-      const command = new DeleteObjectCommand({
-        Bucket: S3_BUCKET,
-        Key: pathname,
-      });
-      const sent = await S3.send(command);
-      console.log(sent);
-      response.statusCode = 200;
-      response.end();
+    const command = new DeleteObjectCommand({
+      Bucket: S3_BUCKET,
+      Key: pathname,
     });
+    await S3.send(command);
+    response.statusCode = 200;
+    response.end();
   } else {
     response.statusCode = 405;
     response.end();
